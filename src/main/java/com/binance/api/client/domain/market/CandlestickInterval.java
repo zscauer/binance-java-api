b6a9fa4a57d5cd416a6f929a1c0abcccf3f3fcae
zscauer <zscauer@gmail.com>
2023-01-31 17:08:44 +0300

    extend Candlestick interval enum

--- src/main/java/com/binance/api/client/domain/market/CandlestickInterval.java
+++ src/main/java/com/binance/api/client/domain/market/CandlestickInterval.java
@@ -10,26 +10,44 @@ import lombok.Getter;
 @Getter
 @JsonIgnoreProperties(ignoreUnknown = true)
 public enum CandlestickInterval {
-  ONE_MINUTE("1m"),
-  THREE_MINUTES("3m"),
-  FIVE_MINUTES("5m"),
-  FIFTEEN_MINUTES("15m"),
-  HALF_HOURLY("30m"),
-  HOURLY("1h"),
-  TWO_HOURLY("2h"),
-  FOUR_HOURLY("4h"),
-  SIX_HOURLY("6h"),
-  EIGHT_HOURLY("8h"),
-  TWELVE_HOURLY("12h"),
-  DAILY("1d"),
-  THREE_DAILY("3d"),
-  WEEKLY("1w"),
-  MONTHLY("1M");
+    ONE_MINUTE("1m"),
+    THREE_MINUTES("3m"),
+    FIVE_MINUTES("5m"),
+    FIFTEEN_MINUTES("15m"),
+    HALF_HOURLY("30m"),
+    HOURLY("1h"),
+    TWO_HOURLY("2h"),
+    FOUR_HOURLY("4h"),
+    SIX_HOURLY("6h"),
+    EIGHT_HOURLY("8h"),
+    TWELVE_HOURLY("12h"),
+    DAILY("1d") {
+        @Override
+        String getDurationIntervalPrefix() {
+            return "";
+        }
+    },
+    THREE_DAILY("3d") {
+        @Override
+        String getDurationIntervalPrefix() {
+            return "";
+        }
+    },
+    WEEKLY("1w"),
+    MONTHLY("1M");
 
-  private final String intervalId;
+    private final String intervalId;
 
-  CandlestickInterval(String intervalId) {
-    this.intervalId = intervalId;
-  }
+    CandlestickInterval(String intervalId) {
+        this.intervalId = intervalId;
+    }
+
+    String getDurationIntervalPrefix() {
+        return "T";
+    }
+
+    public String getDurationIntervalId() {
+        return "P" + getDurationIntervalPrefix() + intervalId;
+    }
 
 }
